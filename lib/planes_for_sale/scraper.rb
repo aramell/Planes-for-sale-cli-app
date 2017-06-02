f420b904d4ca445b3b933b1046501a769991407d
Andrew <github email address>
2017-06-02 15:17:32 -0400

    more scraper methods

--- lib/planes_for_sale/scraper.rb
+++ lib/planes_for_sale/scraper.rb
@@ -16,7 +16,13 @@ class PlanesForSale::Scraper
                   plane.css('.border-bottom').each do |airplane|
                         plane_title = airplane.css('.listing-name').text
                         plane_desc = airplane.css('.equip-details .m-bottom-5').text
+                        dealer_info = airplane.css('.dealer-info .bold').text
+                        dealer_phone = airplane.css('.call-it-listing a').text
                         total_time = airplane.css()
+                        avionics = airplane.css('page.text.match('total')')
+
+
+                        
            
             binding.pry
                       
