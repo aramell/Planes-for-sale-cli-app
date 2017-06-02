3d09a3e7ec367ea5a00e1eda8e3d05798b10d13c
Andrew <github email address>
2017-06-02 14:14:46 -0400

    initial scrapper class build

--- lib/planes_for_sale/scraper.rb
+++ lib/planes_for_sale/scraper.rb
@@ -2,15 +2,26 @@
  require 'nokogiri'
 require 'pry'
 
-class Scraper
+class PlanesForSale::Scraper
 
       Planesforsale_url = 'https://www.controller.com/listings/aircraft/for-sale/list/category/13/aircraft/manufacturer/cessna/model-group/182'
       plane_url = "https://www.controller.com/listings/aircraft/for-sale/*"
 
 
       def self.scrape_plane_page
-         plane_list = Nokogiri::HTML(open("https://www.controller.com/listings/aircraft/for-sale/list/category/13/aircraft/manufacturer/cessna/model-group/182"))
+         doc = Nokogiri::HTML(open("https://www.controller.com/listings/aircraft/for-sale/list/category/13/aircraft/manufacturer/cessna/model-group/182"))
+            doc.search('.listing-name')
+
+            doc.css('.listings-list').each do |plane|
+                  plane.css('.border-bottom').each do |airplane|
+                        plane_title = airplane.css('.listing-name').text
+                        plane_desc = airplane.css('.equip-details .m-bottom-5').text
+                        total_time = airplane.css()
+           
             binding.pry
+                      
+                  end
+            end
       end
     def self.scrape_profile_page(plane_url)
           
--- planes_for_sale.gemspec
+++ planes_for_sale.gemspec
@@ -6,12 +6,12 @@ require 'planes_for_sale/version'
 Gem::Specification.new do |spec|
   spec.name          = "planes_for_sale"
   spec.version       = PlanesForSale::VERSION
-  spec.authors       = ["TODO: Write your name"]
-  spec.email         = ["<github email address>"]
+  spec.authors       = ["Andrew Ramell"]
+  spec.email         = ["<andrew.ramell@gmail.com>"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{}
+  spec.description   = %q{}
+  spec.homepage      = ""
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
